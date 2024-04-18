f0bb6d270b0dd775479090f86d01f71147c074ef
Michael Caiozzo <michael.caiozzo@gmail.com>
2024-04-18 14:12:32 -0400

    added some comments and cleaned alittle

--- esp32/esp_stream/esp_stream.ino
+++ esp32/esp_stream/esp_stream.ino
@@ -14,7 +14,7 @@
 #define LED 2
 #define bounce 150
 
-
+// Set wifi Address
 #define WIFI_SSID "Vandaley Industries"
 #define WIFI_PASSWORD "15Tattergrace48"
 
@@ -24,27 +24,21 @@
 // #define WIFI_SSID "tuiot"
 // #define WIFI_PASSWORD "bruc3l0w3"
 
+//Set redis server
 #define REDIS_ADDR "redis-12305.c270.us-east-1-3.ec2.cloud.redislabs.com"
 #define REDIS_PORT 12305
 #define REDIS_PASSWORD "mJxWBQYqdbSipoLAcc59qUN1zPQdDMmD"
 
+//set streams field
 #define STREAMS_KEY "A-stream"
 #define STREAMS_GROUP_1 "button"
+
 
 bool buttonStateA = 0;
 bool buttonStateB = 0;
 int aCount = 0;
 int bCount =0;
 
-// #define VALIDATE_VECTOR_STRING_RESULT(result, command) \
-//   result = command; \
-//   if(redis->isErrorReturn(result)) { \
-//     Serial.println(">> This was an error <<"); \
-//     Serial.println(result[0]); \
-//   } else { \
-//     Serial.println(">> Success <<"); \
-//     print_vector(result); \
-//   }
 void blink(int led, int dlay){
         digitalWrite(LED, HIGH);
         delay(dlay);
@@ -53,24 +47,26 @@ void blink(int led, int dlay){
 
 void redisXadd(char* but){
   WiFiClient redisConn;
-  if (!redisConn.connect(REDIS_ADDR, REDIS_PORT))
-  {
+  if (!redisConn.connect(REDIS_ADDR, REDIS_PORT)){
+
       Serial.println("Failed to connect to the Redis server!");
       return;
   }
 
   Redis redis(redisConn);
   auto connRet = redis.authenticate(REDIS_PASSWORD);
-  if (connRet == RedisSuccess)
-  {
+
+  if (connRet == RedisSuccess){
+
       Serial.println("Connected to the Redis server!");
   }
-  else
-  {
+
+  else{
+
       Serial.printf("Failed to authenticate to the Redis server! Errno: %d\n", (int)connRet);
       return;
   }
-    if(but == "A"){
+  if(but == "A"){
       aCount= redis.get(but).toInt();
       aCount++;
       String newValue = String(aCount);
@@ -103,6 +99,7 @@ void redisXadd(char* but){
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to the WiFi");
+  
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(250);
@@ -112,11 +109,12 @@ void redisXadd(char* but){
   Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
 
+  //blink 5 times to alert end of setup;
   for(int i = 0 ; i < 5 ; i++){
     blink(2,150);
     delay(100);
   }
-  // redisXadd("A");
+
 }
 
 void loop(){
